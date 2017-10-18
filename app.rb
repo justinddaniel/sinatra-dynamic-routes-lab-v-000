b2aa45a86ccca84709af96d8f789c1eda076fa0f
Justin Daniel <justinddaniel@gmail.com>
2017-10-18 23:16:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    reverse_name = @username.reverse
+    "#{reverse_name}" 
+  end
 
 end
