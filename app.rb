946a69e02007e6020d8b91b7cda2e962242a8870
Justin Daniel <justinddaniel@gmail.com>
2017-10-18 23:33:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,8 +10,15 @@ class App < Sinatra::Base
   get '/square/:number' do
     @number = params[:number]
     number = @number.to_i
-    square = number * number 
+    square = number * number
     "#{square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    number = @number.to_i
+    @phrase * number
   end
 
 end
